8be12c488d8ae761933b5b56d5ab3d5a14651941
Derek <dargall91@comcast.net>
2022-10-07 23:57:00 -0400

    Update WEAMessageAlertArea class to send all info

--- wea/src/main/java/com/capstone/wea/model/WEAMessageAlertArea.java
+++ wea/src/main/java/com/capstone/wea/model/WEAMessageAlertArea.java
@@ -1,6 +1,7 @@
 package com.capstone.wea.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
@@ -9,6 +10,7 @@ import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JacksonXmlRootElement(localName = "CMAC_Alert_Area")
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
 public class WEAMessageAlertArea {
     @JsonProperty("CMAC_area_description")
     private String areaDescription;
@@ -22,6 +24,9 @@ public class WEAMessageAlertArea {
     @JsonProperty("CMAC_cmas_geocode")
     @JacksonXmlElementWrapper(useWrapping = false)
     private List<String> geocodeList;
+    @JsonProperty("CMAC_cap_geocode")
+    @JacksonXmlElementWrapper(useWrapping = false)
+    private List<WEAMessageCapGeocode> capGeocodeList;
 
     public String getAreaDescription() {
         return areaDescription;
