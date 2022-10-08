8a051d3775041338eac8652aeb426004a78d5c27
Derek <dargall91@comcast.net>
2022-10-07 21:57:17 -0400

    Create spring boot project

---
+++ wea/src/main/java/com/capstone/wea/WeaApiApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.wea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeaApiApplication.class, args);
+	}
+
+}
---
+++ wea/src/test/java/com/capstone/wea/WeaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.wea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
