343ff68100bc921c8d2abc0476414867864149be
Battlemuffin <akcordeau@gmail.com>
2022-10-13 19:40:17 -0700

    Add and integrate Header component

--- src/context.jsx
+++ src/context.jsx
@@ -1,6 +1,20 @@
 import React, { useContext, useEffect, useState } from "react";
 
 // any static variables go here
+const monthHolder = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
 
 const AppContext = React.createContext();
 
@@ -8,15 +22,23 @@ const AppContext = React.createContext();
 const AppProvider = ({ children }) => {
   // State Variables
 
+  const [date, setDate] = useState("");
+
   // Functions
+  const getDate = () => {
+    let today = new Date();
+    let day = String(today.getDate());
+    let month = monthHolder[today.getMonth()];
+    let year = today.getFullYear();
+    setDate(`${month} ${day}, ${year}`);
+  };
 
   // useEffects
+  useEffect(() => {
+    getDate();
+  });
 
-  return (
-    <AppContext.Provider value={{ ThisIs: "Placeholder" }}>
-      {children}
-    </AppContext.Provider>
-  );
+  return <AppContext.Provider value={{ date }}>{children}</AppContext.Provider>;
 };
 
 export const useGlobalContext = () => {
