a6a9cdedc608ffb2371097c2c50b7483e748dddf
Derek <dargall91@comcast.net>
2022-10-14 19:41:53 -0400

    Update user data model and endpoint

--- wea/src/main/java/com/capstone/wea/controller/WEAController.java
+++ wea/src/main/java/com/capstone/wea/controller/WEAController.java
@@ -1,8 +1,7 @@
 package com.capstone.wea.controller;
 
+import com.capstone.wea.model.CollectedUserData;
 import com.capstone.wea.model.WEAMessageModel;
-import com.capstone.wea.model.wrappers.UploadWrapper;
-import com.ctc.wstx.shaded.msv.org_jp_gr_xml.dom.XMLMaker;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import com.fasterxml.jackson.dataformat.xml.ser.ToXmlGenerator;
 import org.springframework.http.HttpStatus;
@@ -12,15 +11,13 @@ import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.io.File;
-import java.io.IOException;
 import java.net.URI;
 import java.util.HashMap;
-import java.util.List;
 
 @RequestMapping("/wea")
 @RestController
 public class WEAController {
-    private HashMap<Integer, UploadWrapper> uploads;
+    private HashMap<Integer, CollectedUserData> uploads;
 
     public WEAController() {
         uploads = new HashMap<>();
@@ -56,27 +53,29 @@ public class WEAController {
      * Endpoint to which the data collected from the
      * mobile device will be sent
      *
-     * @param wrapper An xml body containing a
-     *                WEAMessageModel and a
-     *                CollectedUserData
+     * @param userData An xml body containing the data
+     *                 collected from the user
+     *
      * @return HTTP 201 CREATED and the URI of the
      *         uploaded data
      */
     @PutMapping(value = "/upload")
-    public ResponseEntity<String> upload(@RequestBody UploadWrapper wrapper) {
+    public ResponseEntity<String> upload(@RequestBody CollectedUserData userData) {
         URI location = ServletUriComponentsBuilder
-                .fromHttpUrl("http://localhost:8080/wea/getUpload?identifier=" + wrapper.getId())
+                .fromHttpUrl("http://localhost:8080/wea/getUpload?identifier=" + userData.getId())
                 .buildAndExpand()
                 . toUri();
 
-        uploads.put(wrapper.getId(), wrapper);
+        uploads.put(userData.getId(), userData);
 
         return ResponseEntity.created(location).build();
     }
 
     /**
-     * Gets a data upload represented by a
-     * unique identifier
+     * Gets a data upload represented by a unique
+     * identifier. This endpoint is only used to
+     * confirm successful uploads from devices
+     * during testing
      *
      * @param identifier Unique upload identifier
      * @return HTTP 200 OK and the uploaded data
@@ -84,14 +83,14 @@ public class WEAController {
      *         FOUND if the identifier is invalid
      */
     @GetMapping(value = "/getUpload", produces = "application/xml")
-    public ResponseEntity<UploadWrapper> getUpload(@RequestParam int identifier) {
+    public ResponseEntity<CollectedUserData> getUpload(@RequestParam int identifier) {
         XmlMapper mapper = null;
-        UploadWrapper wrapper = uploads.get(identifier);
+        CollectedUserData data = uploads.get(identifier);
 
-        if (wrapper == null) {
+        if (data == null) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Upload not found");
         }
 
-        return ResponseEntity.ok(wrapper);
+        return ResponseEntity.ok(data);
     }
 }
--- wea/src/main/java/com/capstone/wea/model/CollectedUserData.java
+++ wea/src/main/java/com/capstone/wea/model/CollectedUserData.java
@@ -7,7 +7,27 @@ import java.time.LocalDateTime;
 @JacksonXmlRootElement(localName = "CMAC_user_data")
 public class CollectedUserData {
     @JsonProperty("CMAC_user_time_received")
-    private LocalDateTime received;
-    @JsonProperty("CMAC_user_location")
-    private String location;
+    private LocalDateTime timeReceived;
+    @JsonProperty("CMAC_user_time_displayed")
+    private LocalDateTime timeDisplayed;
+    @JsonProperty("CMAC_user_location_received")
+    private String locationReceived;
+    @JsonProperty("CMAC_user_location_displayed")
+    private String locationDisplayed;
+    @JsonProperty("CMAC_message_number")
+    private String messageNumber;
+    @JsonProperty("CMAC_cap_identifier")
+    private String capIdentifier;
+
+    //identifier for upload confirmation testing
+    private int id;
+    private static int next_id = 0;
+
+    public CollectedUserData() {
+        id = next_id++;
+    }
+
+    public int getId() {
+        return id;
+    }
 }
