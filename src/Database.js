be44f61a018a821f8dc16e26ca3633ba4c322eab
Derek <dargall91@comcast.net>
2022-10-16 14:21:43 -0400

    Successfully connect to wea api

--- src/Database.js
+++ src/Database.js
@@ -1,10 +1,16 @@
-var baseUrl = "http://localhost:8080/wea/"
+var baseUrl = "http://localhost:8080/wea/";
 
-async function getMessageList(ao) {
-    const result = await fetch(`${baseUrl}getMessageList?sender=${ao}`)
-    const data = await result.json();
-
-    console.log(data);
+const Database = {
+    /**
+     * Fetches a list of cmac_message_numbers and their cmac_sent_date_times
+     * that belong to a specified AO from the server
+     * 
+     * @param {string} ao The AO who's messages should be fetched
+     */
+    async getMessageList(ao) {
+        const result = await fetch(`${baseUrl}getMessageList?sender=${ao}`);
+        const data = await result.json();
+    }
 }
 
-module.exports = "Database";
+module.exports = Database;
