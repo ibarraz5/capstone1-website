8cca9c266f3a6bd89daaf14a054fd86643ba9669
Derek <dargall91@comcast.net>
2022-10-21 20:32:06 -0400

    Update function descriptions

--- src/DbConn.js
+++ src/DbConn.js
@@ -2,10 +2,12 @@ const baseUrl = "http://localhost:8080/wea/";
 
 /**
  * Fetches a list of cmac_message_numbers and their cmac_sent_date_times
- * that belong to a specified AO from the server. Use the results to
- * create a list of message cards for the user to select.
+ * that belong to a specified AO from the server. Call this function
+ * when the user logs in to get their list of messages and use the list
+ * to create the cards
  * 
- * @param {string} ao The AO who's messages should be fetched
+ * @param {string} ao The AO who's messages should be fetched.
+ *                    '@' characters must be encoded as "%40"
  */
 export const getMessageList = async (ao) => {
     console.log(ao);
@@ -17,8 +19,8 @@ export const getMessageList = async (ao) => {
 
 /**
  * Fetches the stats for all the devices that received a given message.
- * Call this message when a card is opened to get the stats for that
- * card.
+ * Call this function when a card is opened to get the stats to display
+ * on that card.
  * 
  * @param {string} msgNum The cmac_message_number of the message to get
  *                        the stats for
