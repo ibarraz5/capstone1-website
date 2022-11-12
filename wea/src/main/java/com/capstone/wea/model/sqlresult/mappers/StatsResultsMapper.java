afa6cafc519bba71dd7e037e1bd67081f17a05e5
dargall91 <dargall91@comcast.net>
2022-11-12 11:27:32 -0500

    Add new data fields to response body

--- wea/src/main/java/com/capstone/wea/model/sqlresult/mappers/StatsResultsMapper.java
+++ wea/src/main/java/com/capstone/wea/model/sqlresult/mappers/StatsResultsMapper.java
@@ -14,11 +14,16 @@ public final class StatsResultsMapper implements RowMapper<MessageStatsResult> {
     @Override
     public MessageStatsResult mapRow(ResultSet rs, int rowNum) throws SQLException {
         result.setDate(rs.getString("CMACDateTime"));
+        result.setMessageType(rs.getString("CMACMessageType"));
         result.setDeviceCount(rs.getInt("DeviceCount"));
         result.setAvgTime(rs.getTime("AvgTime"));
         result.setLongTime(rs.getTime("LongTime"));
         result.setShortTime(rs.getTime("ShortTime"));
         result.setAvgDelay(rs.getTime("AvgDelay"));
+        result.setReceivedOutsideCount(rs.getInt("ReceivedOutsideCount"));
+        result.setDisplayedOutsideCount(rs.getInt("DisplayedOutsideCount"));
+        result.setReceivedAfterExpiredCount(rs.getInt("ReceivedExpiredCount"));
+        result.setDisplayedAfterExpiredCount(rs.getInt("DisplayedExpiredCount"));
 
         return result;
     }
